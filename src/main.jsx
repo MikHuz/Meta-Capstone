ece191ef0328e81afe2c5b92f9567d7636a0d135
Mikhuz <michaelhuziy@gmail.com>
2025-07-30 17:05:09 -0700

    2 Half Finished Pages Created

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter>
     <App />
-  </StrictMode>,
+    </BrowserRouter>
+  </StrictMode>
+  
 )
